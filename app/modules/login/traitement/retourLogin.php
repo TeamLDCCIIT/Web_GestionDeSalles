bd25853aeae5254de178e40d5bc68d7f91100fe5
Unknown <jeremie.soulard@reseau.eseo.fr>
2018-01-15 11:29:48 +0100

    retourLogin

--- app/modules/login/traitement/retourLogin.php
+++ app/modules/login/traitement/retourLogin.php
@@ -1,54 +1,33 @@
 <?php
 /**
- * Created by Tristan LE GACQUE on 15/01/2018
+ * Created by Jérémie on 15/01/2018
  */
 
-$login = isset($_GET['login']) ? strval($_GET['login']) : null;
-$mdp = isset($_GET['mdp']) ? strval($_GET['mdp']) : null;
+$login = isset($_GET['InputUsername']) ? strval($_GET['InputUsername']) : null;
+$mdp = isset($_GET['InputPassword']) ? strval($_GET['InputPassword']) : null;
+echo $login;
+
+$db = new PgSqlLib();
+$req = "SELECT groupe FROM Utilisateur WHERE Utilisateur.login = '".$login."' AND Utilisateur.password = '".$mdp."'";
+$result = $db->query($req);
+
+if(!$result){
+    $type_res = 'error';
+    $msg = "login error";
+} else {
+    $resultat = $result->fetch_assoc();
+    $groupe = $resultat['groupe'];
+    $_SESSION['user']['username'] = $login;
+    $_SESSION['user']['group'] = array($groupe);
+    $type_res = 'success';
+    $msg = "PAPA DANS MAMAN ";
+    $db->close();
+};
 
 
-<html>
-	<head>
-		<meta charset="utf-8"/>
-		<title>Identifié</title>
-	</head>
-	<body>
-		<?php $login = $_GET['login']; $mdp = $_GET['mdp']; ?>
-		<label>Votre login :</label>
-		<?php echo $login ?>
-		</br>
-		<label>Votre mot de passe :</label>
-		<?php echo $mdp ?>
-		</br>
-		<?php $req = "SELECT login, motDePasse, numUtilisateur FROM Utilisateur WHERE Utilisateur.login = '".$login."' AND Utilisateur.motDePasse = '".$mdp."'"; ?>
-		</br>
-		<?php
-				$connexion = new mysqli("localhost", "gpi2", "network", "projet");
-				if ($connexion->connect_errno) {
-					printf("Echec de la connexion : %s %s", $connexion->connect_errno, $connexion->connect_errno);
-					exit();
-				}
-				$connexion->set_charset("utf-8");
-				$result = $connexion->query($req);
-				if (!$result) {
-					echo "la requête ne s'est pas éxécutée </br>";
-				} else {
-					echo "la requête s'est bien passée</br>";
-					$resultat = $result->fetch_assoc();
-					$numeroUtilisateur = $resultat["numUtilisateur"];
-					session_start();
-					$_SESSION[’utilisateur’] = $numeroUtilisateur;
-					if ($resultat["login"] == "root" && $resultat["motDePasse"] == "network") {
-						echo "<a href=menuAdministrateur.php>Menu administrateur</a>";
+$response = array(
+        "type"  =>  $type_res, //(error)
+        "message"   =>  $msg
+);
 
-					} else {
-						echo "<a href=listeConcours.php>Liste des concours</a>";
-					}
-					$connexion->free();
-				}
-				$connexion->close();
-		?>
-		</br>
-
-	</body>
-</html>
+die(json_encode($response));
