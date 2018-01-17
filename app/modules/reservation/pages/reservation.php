88d17930199c240adfe1b6c46bba306cae85fb62
Pingoo <tristanlegacque@orange.fr>
2018-01-17 11:35:37 +0100

    Amélioration du visuel et automatisation

--- app/modules/reservation/pages/reservation.php
+++ app/modules/reservation/pages/reservation.php
@@ -6,6 +6,21 @@ $template = new \Tpl\Template(__DIR__ . '/../templates/reservation.html');
 $__template->setVar('pagetitle', 'Réservation');
 $__template->setVar('page_title', 'Réservation');
 
+//récupération des campus
+$campus = array();
+$db = new PgSqlLib();
+$queryCampus    = "SELECT id_campus, nom FROM campus";
+
+//Campus
+$result = $db->query($queryCampus);
+while($row = $result->fetch_assoc()) {
+    array_push($campus, array(
+        'key'   => $row['id_campus'],
+        'value' => $row['nom']
+    ));
+}
+
+$template->setVar('campus', $campus);
 
 //FIN - Render du template (compiler le template et l'afficher)
 $template->render($__template, 'content');
