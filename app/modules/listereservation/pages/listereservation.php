d36594ee802708e5a862b51bddb326e4a2ffefc6
Pingoo <tristanlegacque@orange.fr>
2018-01-18 15:55:15 +0100

    Done - Ajout d'un bouton de suppression des reservations

--- app/modules/listereservation/pages/listereservation.php
+++ app/modules/listereservation/pages/listereservation.php
@@ -3,8 +3,8 @@
 $template = new \Tpl\Template(__DIR__ . '/../templates/listereservation.html');
 
 //Définition des variables générales
-$__template->setVar('pagetitle', 'Liste des réservations');
-$__template->setVar('page_title', 'Liste des réservations');
+$__template->setVar('pagetitle', 'Mes réservations');
+$__template->setVar('page_title', 'Liste de mes réservations');
 
 //Récupération des réservations d'un utilisateur
 $db = new PgSqlLib();
@@ -15,21 +15,28 @@ $reservations = \lib\objets\Reservation::getUserReservations($db, $utilisateur->
 //Trier par date
 //format : date => (reservations), ...
 $reservations_tri = array();
+$reservations_out = array();
 
+$today = new DateTime();
 foreach($reservations as $resa) {
     $date = new DateTime($resa->getDateDebut());
-    $ddmmyyyy = $date->format('Y/m/d');
-    if(!isset($reservations_tri[$ddmmyyyy]) || !is_array($reservations_tri[$ddmmyyyy])) {
-        $reservations_tri[$ddmmyyyy] = array();
-    }
+    $ddmmyyyy = $date->format('d M Y');
+
     //ajout de la réservation
-    array_push($reservations_tri[$ddmmyyyy], $resa);
+    if($date >= $today) {
+        if(!isset($reservations_tri[$ddmmyyyy]) || !is_array($reservations_tri[$ddmmyyyy])) {
+            $reservations_tri[$ddmmyyyy] = array();
+        }
+        array_push($reservations_tri[$ddmmyyyy], $resa);
+    } else {
+        if(!isset($reservations_out[$ddmmyyyy]) || !is_array($reservations_out[$ddmmyyyy])) {
+            $reservations_out[$ddmmyyyy] = array();
+        }
+    }
+
 }
 
 $template->setVar('reservations', $reservations_tri);
-//Faire en TWIG
-//TODO
-
 
 //FIN - Render du template (compiler le template et l'afficher)
 $template->render($__template, 'content');
