36ad354c20ef0310d94db0cd16636b2c564cae83
Unknown <scapinz@orange.fr>
2018-01-18 10:05:49 +0100

    amélioration du calendrier et du logout

--- app/modules/listereservation/js/script.js
+++ app/modules/listereservation/js/script.js
@@ -5,31 +5,8 @@
 $(function() {
 	
 	// this will use clndr's default template, which you probably don't want.
-$('#calendar').clndr();
+$('#full-clndr').clndr();
 
-// so instead, pass in your template as a string!
-$('#calendar').clndr({
-  template: $('#calendar-template').html()
-});
-
-// there are a lot of options. the rabbit hole is deep.
-$('#calendar').clndr({
-  template: $('#calendar-template').html(),
-  events: [
-    { date: '2013-09-09', title: 'CLNDR GitHub Page Finished', url: 'http://github.com/kylestetz/CLNDR' }
-  ],
-  clickEvents: {
-    click: function(target) {
-      console.log(target);
-    },
-    onMonthChange: function(month) {
-      console.log('you just went to ' + month.format('MMMM, YYYY'));
-    }
-  },
-  doneRendering: function() {
-    console.log('this would be a fine place to attach custom event handlers.');
-  }
-});
  
 });
 
